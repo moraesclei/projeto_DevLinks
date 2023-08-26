254c130afee72b9eed02291003f7278d2d37989e
Moraes Clei <moraesclei@gmail.com>
2023-08-25 23:42:26 -0300

    update in directory script in the html and create functions for the pictures

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 function toggleMode(){
+    // aqui estou pegando a instrução html
     const html = document.documentElement
+    html.classList.toggle('light')
+ // ######## este é uma outra forma de fazer a alteração utilizando o if, else
 
     // if(html.classList.contains('light')){
     //     html.classList.remove('light')
@@ -7,5 +10,17 @@ function toggleMode(){
     //     html.classList.add('light')
     // }
 
-    html.classList.toggle('light')
+// #########
+    
+    // pegar a tag img
+    const img = document.querySelector('#profile img')
+    
+    // substituir a imagem
+    if(html.classList.contains('light')){
+        // se tiver light mode, adicionar a imagem light
+        img.setAttribute('src', './assets/avatar_clei.png')
+    } else{
+        // se tiver sem light mode, manter a imagem normal
+        img.setAttribute('src', './assets/avatar_clei.png')
+    }
 }
